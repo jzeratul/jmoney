654f137b5b470998d715c988afb26b6a87d31ff3
jzeratul <vlad.vesa@gmail.com>
2020-10-19 15:39:34 +0200

    moving classes around

--- jmoney-server/src/main/java/com/jzeratul/jmoneyserver/repositories/JUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jzeratul.jmoneyserver.repositories;
-
-import com.jzeratul.jmoneyserver.model.JUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JUserRepo extends JpaRepository<JUser, Long> {
-}
