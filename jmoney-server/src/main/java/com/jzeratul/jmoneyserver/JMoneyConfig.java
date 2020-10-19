8447edf40b16a8ad0fe3b165e78c21cbf814ff2e
jzeratul <vlad.vesa@gmail.com>
2020-10-19 15:39:34 +0200

    moving classes around

--- jmoney-server/src/main/java/com/jzeratul/jmoneyserver/JMoneyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jzeratul.jmoneyserver;
-
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableEncryptableProperties
-public class JMoneyConfig {
-}
--- jmoney-server/src/main/java/com/jzeratul/jmoneyserver/repositories/IncomeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jzeratul.jmoneyserver.repositories;
-
-import com.jzeratul.jmoneyserver.model.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IncomeRepo extends JpaRepository<Income, Long> {
-}
