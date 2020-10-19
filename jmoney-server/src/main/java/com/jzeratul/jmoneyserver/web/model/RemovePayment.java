12bb7955d2681a63f5bbde7f80cd12bc4c83f17f
jzeratul <vlad.vesa@gmail.com>
2020-10-19 15:24:27 +0200

    a proposal for the frontend calls: once the user is done with add/remove items from the jars, submit the entire object. Might configure this later

---
+++ jmoney-server/src/main/java/com/jzeratul/jmoneyserver/web/model/RemovePayment.java
@@ -0,0 +1,18 @@
+package com.jzeratul.jmoneyserver.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//TODO move to swagger
+@Builder(toBuilder = true)
+public class RemovePayment {
+
+  private long jarId;
+	private long paymentId;
+
+}
